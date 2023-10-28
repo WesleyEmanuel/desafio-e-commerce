bb1d2ede79d69cbd65aadd50cea6b3cf0d931def
Wesley Emanuel <wesleyemanuel.1517@gmail.com>
2023-10-28 08:37:06 -0300

    ajuste variaveis de ambiente

--- src/utils/enum.js
+++ src/utils/enum.js
@@ -1,5 +1,5 @@
 export const DEFAULT_API_URLS = {
-  LOCAL: '',
-  PRODUCTION: '',
+  LOCAL: 'https://www.cheapshark.com/api/1.0/',
+  PRODUCTION: 'https://www.cheapshark.com/api/1.0/',
   DEVELOPMENT: 'https://www.cheapshark.com/api/1.0/'
 };
